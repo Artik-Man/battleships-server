8d7583c7ed9fee970f8897975ce2e94eb542f28a
Artik <artik.alpha@gmail.com>
2019-04-11 00:38:54 +0300

    UPD: server

--- README.md
+++ README.md
@@ -10,9 +10,32 @@ https://github.com/Artik-Man/ws-post-server
 ## Connection
 1. Create connection to server
 ```javascript
-const socket = new WebSocket('wss://[HOST]:[PORT]/[USER]');
+const socket = new WebSocket('wss://ws-post.herokuapp.com/');
 ```
-2. Send message
+You will get your ID in first message from server:
+```json
+{
+    "from":"SERVER",
+    "id":  "YOUR_ID"
+}
+```
+2. Get connections
+```javascript
+const message = JSON.stringify({
+    to:   "SERVER",
+    data: "connections"
+});
+socket.send(message);
+```
+You will get current connections:
+```json
+{
+    "from":"SERVER",
+    "connections":  [USER1_ID, USER2_ID]
+}
+```
+
+3. Send message
 ```javascript
 const message = JSON.stringify({
     to:   SOME_USER,
@@ -23,32 +46,33 @@ socket.send(message);
 
 ## Simple chat
 ```javascript
-const socket = new WebSocket('wss://[HOST]:[PORT]/[USER1]');
-
-socket.onmessage = function(resp) {
-    const message = JSON.parse(resp.data);
-    console.log("New message from: " + message.from + ": " + message.data);
-}
+const socket = new WebSocket('wss://ws-post.herokuapp.com/');
+let users = [];
 
 function send(to, data) {
     const message = { to, data };
     socket.send(JSON.stringify(message));
 }
 
-send(USER2, DATA);
-```
+function startChat() {
+    users.forEach(user => {
+        send(user, "hello");
+    });
+}
 
-## Additionals
-1. `GET /` 
+socket.onopen = function() {
+    send('SERVER', "connections");
+}
 
-Response: [ HTML ] docs and autotest
-
-2. `GET /login`
-
-Response: [ JSON ] 
-```javascript
-{
-    "id":   "YOUR_ID",
-    "port": "CURRENT_PORT"
+socket.onmessage = function(resp) {
+    const message = JSON.parse(resp.data);
+    if (resp.data.from === 'SERVER') {
+        if (resp.data.connections) {
+            users = resp.data.connections;
+            startChat();
+        }
+    } else {
+        console.log("New message from: " + message.from + ": " + message.data);
+    }
 }
 ```
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ showdown.setFlavor('github');
 const converter = new showdown.Converter();
 const expressWs = expressWS(express());
 const app = expressWs.app;
-const server = app.listen(process.env.PORT || null);
+const server = app.listen(process.env.PORT || null || 3000);
 const port = server.address()['port'];
 const connections = {};
 console.log('Server running on port: ' + port);
@@ -33,6 +33,14 @@ const parseData = (message, from) => {
     }
 };
 
+const broadcast = (message, without) => {
+    Object.values(connections).forEach(ws => {
+        if (without !== ws) {
+            ws.send(JSON.stringify(message));
+        }
+    });
+}
+
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
@@ -40,20 +48,19 @@ app.use(function (req, res, next) {
 
 app.use(helmet());
 
-app.param('id', function (req, res, next, id) {
-    req['id'] = id || '';
-    return next();
-});
+// app.param('id', function (req, res, next, id) {
+//     req['id'] = id || '';
+//     return next();
+// });
 
-app.get('/login', function (req, res, next) {
-    let id;
-    do {
-        id = uuidv4();
-    }
-    while (!!connections[id])
-    res.json({ port, id });
-    next();
-});
+// app.get('/login', function (req, res, next) {
+//     let id;
+//     do {
+//         id = uuidv4();
+//     } while (!!connections[id])
+//     res.json({ port, id });
+//     next();
+// });
 
 app.get('/', function (req, res, next) {
     const files = ['/index.html', '/README.md', '/footer.html'];
@@ -82,33 +89,79 @@ app.get('/', function (req, res, next) {
     });
 });
 
-app.ws('/:id', function (ws, req, next) {
-    const userID = req['id'] || '';
-    if (!userID.length || connections[userID] || Object.keys(connections).length > PARAMS.MAX_CLIENTS) {
+// app.ws('/:id', function (ws, req, next) {
+//     const userID = req['id'] || '';
+//     if (!userID.length || connections[userID] || Object.keys(connections).length > PARAMS.MAX_CLIENTS) {
+//         ws.send(JSON.stringify({ error: 'Can not connect' }));
+//         console.warn('Can not connect');
+//         ws.close();
+//         return;
+//     }
+//     connections[userID] = ws;
+//     console.log('Connected: ' + userID);
+//     ws.on('message', function (message) {
+//         const msg = parseData(message, userID);
+//         if (msg && connections[msg.to]) {
+//             connections[msg.to].send(JSON.stringify(msg));
+//             console.log('Send message: ' + JSON.stringify(msg));
+//         } else if (msg) {
+//             setTimeout(() => {
+//                 connections[userID].send(JSON.stringify(Object.assign({}, msg, { error: 'Connection not found: ' + msg.to })));
+//             }, 1000);
+//         } else {
+//             connections[userID].send(JSON.stringify({ error: 'Parse error' }));
+//         }
+//     });
+//     ws.on('close', function (msg) {
+//         delete connections[userID];
+//         console.log('Close connection: ' + userID);
+//     });
+//     next();
+// });
+
+app.ws('/', function (ws, req, next) {
+    const userID = uuidv4();
+
+    if (connections[userID] || Object.keys(connections).length > PARAMS.MAX_CLIENTS) {
         ws.send(JSON.stringify({ error: 'Can not connect' }));
-        console.warn('Can not connect');
+        console.warn('Can not connect:"' + userID + '"; ' + Object.keys(connections).length);
         ws.close();
         return;
     }
+
+    ws.send(JSON.stringify({ from: 'SERVER', data: { id: userID } }));
     connections[userID] = ws;
     console.log('Connected: ' + userID);
+    broadcast({ from: 'SERVER', data: { login: userID } }, ws);
+
     ws.on('message', function (message) {
         const msg = parseData(message, userID);
-        if (msg && connections[msg.to]) {
+        if (msg && msg.to === 'SERVER') {
+            const response = { from: 'SERVER' };
+            if (msg.data === 'ping') {
+                response.data = 'pong';
+            } else if (msg.data === 'connections') {
+                response.data = { connections: Object.keys(connections) }
+            }
+            ws.send(JSON.stringify(response));
+        } else if (msg && connections[msg.to]) {
             connections[msg.to].send(JSON.stringify(msg));
             console.log('Send message: ' + JSON.stringify(msg));
-        } else if (msg) {
+        } else if (msg && !connections[msg.to]) {
             setTimeout(() => {
                 connections[userID].send(JSON.stringify(Object.assign({}, msg, { error: 'Connection not found: ' + msg.to })));
             }, 1000);
         } else {
-            connections[userID].send(JSON.stringify({ error: 'Parse error' }));
+            connections[userID].send(JSON.stringify(Object.assign({}, msg, { error: 'Parse error. Message must have "to:string" and "data:any" fields' })));
         }
     });
+
     ws.on('close', function (msg) {
         delete connections[userID];
         console.log('Close connection: ' + userID);
+        broadcast({ from: 'SERVER', data: { logout: userID } });
     });
+
     next();
 });
 
